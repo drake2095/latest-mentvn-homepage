745ef339c834af9fd1f65346f3f6295357b78335
drake2095 <113614020+drake2095@users.noreply.github.com>
2022-09-16 12:01:57 +0700

    4 files modified, 1 added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  build: {
+    transpile: [
+        '@fortawesome/fontawesome-svg-core',
+        '@fortawesome/pro-solid-svg-icons',
+        '@fortawesome/pro-regular-svg-icons',
+        '@fortawesome/pro-light-svg-icons',
+        '@fortawesome/free-brands-svg-icons'
+    ]
+}
 })

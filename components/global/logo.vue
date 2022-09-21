8a842f8cb2ac8c6b1f6c3517e0d77b7e8ed8ade6
drake2095 <113614020+drake2095@users.noreply.github.com>
2022-09-21 10:42:21 +0700

    1 file modified

--- components/global/logo.vue
+++ components/global/logo.vue
@@ -1,3 +1,3 @@
 <template>
-   <NuxtLink class="logo-mentvn" to="/#"><img src="https://stackblitz.com/files/latest-mentvn-homepage/github/drake2095/latest-mentvn-homepage/main/assets/logo-white.png" alt="LogoMentVN"/></NuxtLink>
+   <NuxtLink class="logo-mentvn" to="/#"><img src="https://stackblitz.com/files/latest-mentvn-homepage/github/drake2095/latest-mentvn-homepage/assets/image/logo-white.png" alt="LogoMentVN"/></NuxtLink>
 </template>
